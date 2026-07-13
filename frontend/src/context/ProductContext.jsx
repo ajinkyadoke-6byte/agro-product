1575068dded60b593118286feab4c29330fd5b64
sharuk1807 <sharuk1875@gmail.com>
2026-07-13 23:13:09 +0530

    token added

--- frontend/src/context/ProductContext.jsx
+++ frontend/src/context/ProductContext.jsx
@@ -1,27 +1,22 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
-// import '@tabler/icons-webfont/dist/tabler-icons.min.css'
 
 export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
-
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState(localStorage.getItem("token") || "");
 
     const getProducts = async () => {
         try {
-
             const response = await axios.get(
                 backendUrl + "/api/product/list"
             );
-
             if (response.data.success) {
                 setProducts(response.data.products);
             }
-
         } catch (error) {
             console.log(error);
         } finally {
@@ -33,11 +28,21 @@ const ProductProvider = ({ children }) => {
         getProducts();
     }, []);
 
+    useEffect(() => {
+        if (token) {
+            localStorage.setItem("token", token);
+        } else {
+            localStorage.removeItem("token");
+        }
+    }, [token]);
+
     const value = {
         products,
         loading,
         backendUrl,
         getProducts,
+        token,
+        setToken,
     };
 
     return (
